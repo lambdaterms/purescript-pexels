38bd0132c799c5c49848175ee296236190f94dce
mdz <mikolaj.dzieciolowski@gmail.com>
2018-06-13 12:22:48 +0200

    refactoring of type signatures in validation

--- src/API/Pexels/Validation.purs
+++ src/API/Pexels/Validation.purs
@@ -2,13 +2,13 @@ module API.Pexels.Validation where
 
 import Prelude
 
+import API.Pexels.Search (CuratedPhotos, SearchPhotos, Photo)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Error.Class (catchError)
 import Data.Argonaut (Json, jsonParser)
 import Data.Array (singleton)
 import Data.Either (Either(..))
 import Data.Functor.Variant (SProxy(..))
-import Data.List (List)
 import Data.Maybe (Maybe(..))
 import Data.Record.Fold (collect)
 import Data.Variant (Variant, inj)
@@ -17,10 +17,10 @@ import Network.HTTP.Affjax.Request (class Requestable)
 import Network.HTTP.Affjax.Response (class Respondable)
 import Network.HTTP.StatusCode (StatusCode(..))
 import Polyform.Validation (V(Invalid, Valid), Validation, hoistFnMV, hoistFnV)
-import Validators.Json (arrayOf, field, int, optionalField, string)
+import Validators.Json (JsError, arrayOf, field, int, optionalField, string)
 
 
-type HttpResponseErrorRow (err :: # Type) = (wrongHttpStatus :: StatusCode | err)
+type HttpErrorRow (err :: # Type) = (wrongHttpStatus :: StatusCode | err)
 type JsonErrorRow (err :: # Type) = (parsingError :: String | err)
 type AffjaxErrorRow (err :: # Type) = (remoteError :: String | err)
 
@@ -28,78 +28,42 @@ type AffjaxErrorRow (err :: # Type) = (remoteError :: String | err)
 
 
 
-validateAffjax :: forall t16 t17 t18 err.
-  Requestable t17 => Respondable t16 => Validation
-                                          (Aff ( ajax :: AJAX | t18))
+validateAffjax :: forall req res ext err.
+  Requestable req => Respondable res => Validation
+                                          (Aff ( ajax :: AJAX | ext))
                                           (Array (Variant (AffjaxErrorRow err)))
-                                          (AffjaxRequest t17)
-                                          (AffjaxResponse t16)
+                                          (AffjaxRequest req)
+                                          (AffjaxResponse res)
 validateAffjax = hoistFnMV $ \req → do
     (Valid [] <$> affjax req) `catchError` (\e -> pure (Invalid $ singleton $ (inj (SProxy :: SProxy "remoteError") $ show e)))
 
 
-validateStatus :: forall t1 t18 t6.
-  Monad t1 => Validation t1
-                (Array
-                   (Variant
-                      ( wrongHttpStatus :: StatusCode
-                      | t18
-                      )
-                   )
-                )
-                { status :: StatusCode
-                | t6
-                }
-                { status :: StatusCode
-                | t6
-                }
-                
+validateStatus :: forall m err res.
+  Monad m => Validation m
+                (Array (Variant (HttpErrorRow err)))
+                (AffjaxResponse res)
+                (AffjaxResponse res)
 validateStatus = hoistFnV (\response -> case response.status of
     StatusCode 200 -> Valid [] response
     _ -> Invalid $ singleton $ (inj (SProxy :: SProxy "wrongHttpStatus") response.status))
 
 
 
-getJson :: forall t59 t63 t79 err.
-  Monad t59 => Validation t59
-                 (Array
-                    (Variant
-                       ( JsonErrorRow err)
-                    )
-                 )
-                 { response :: String
-                 | t63
-                 }
+getJson :: forall m err.
+  Monad m => Validation m
+                 (Array (Variant (JsonErrorRow err)))
+                 (AffjaxResponse String)
                  Json
 getJson = hoistFnV \response -> case jsonParser response.response of
   Right js -> Valid [] js
   Left error -> Invalid  $ singleton $ (inj (SProxy :: SProxy "parsingError") error)
 
 
-getSearchResultfromJson :: forall t143 t144.
-  Monad t144 => Validation t144
-                  (Array
-                     (Variant
-                        ( jsError :: { path :: List String
-                                     , msg :: String
-                                     }
-                        | t143
-                        )
-                     )
-                  )
+getSearchResultfromJson :: forall err m.
+  Monad m => Validation m
+                  (Array (Variant (JsError err) ))
                   Json
-                  { totalResults :: Int
-                  , prevPage :: Maybe String
-                  , photos :: Array
-                                { width :: Int
-                                , url :: String
-                                , id :: Int
-                                , height :: Int
-                                }
-                  , perPage :: Int
-                  , page :: Int
-                  , nextPage :: Maybe String
-                  }
+                  SearchPhotos
 getSearchResultfromJson = collect
   { totalResults: field "total_results" int
   , nextPage: optionalField "next_page" (Just <$> string)
@@ -109,29 +73,11 @@ getSearchResultfromJson = collect
   , photos: field "photos" $ arrayOf getPhotosfromJson
 }
 
-getCuratedResultfromJson :: forall t286 t287.
-  Monad t287 => Validation t287
-                  (Array
-                     (Variant
-                        ( jsError :: { path :: List String
-                                     , msg :: String
-                                     }
-                        | t286
-                        )
-                     )
-                  )
+getCuratedResultfromJson :: forall err m.
+  Monad m => Validation m
+                  (Array (Variant (JsError err) ))
                   Json
-                  { prevPage :: Maybe String
-                  , photos :: Array
-                                { width :: Int
-                                , url :: String
-                                , id :: Int
-                                , height :: Int
-                                }
-                  , perPage :: Int
-                  , page :: Int
-                  , nextPage :: Maybe String
-                  }
+                  CuratedPhotos
 
 getCuratedResultfromJson = collect
   { nextPage: optionalField "next_page" (Just <$> string)
@@ -141,23 +87,11 @@ getCuratedResultfromJson = collect
   , photos: field "photos" $ arrayOf getPhotosfromJson
 }
 
-getPhotosfromJson :: forall t77 t78.
-  Monad t78 => Validation t78
-                 (Array
-                    (Variant
-                       ( jsError :: { path :: List String
-                                    , msg :: String
-                                    }
-                       | t77
-                       )
-                    )
-                 )
-                 Json
-                 { width :: Int
-                 , url :: String
-                 , id :: Int
-                 , height :: Int
-                 }
+getPhotosfromJson :: forall err m.
+  Monad m => Validation m
+                  (Array (Variant (JsError err) ))
+                  Json
+                  Photo
 getPhotosfromJson = collect
   { id: field "id" int
   ,  width: field "width" int
